d68e8d41d791af4840a88586ad7eed7a64df00fa
Rameez <rameez.arif1@gmail.com>
2024-08-29 21:11:03 -0400

    start impl creating reservation

--- src/main/java/com/rameez/restaurant/api/request/ReservationRequest.java
+++ src/main/java/com/rameez/restaurant/api/request/ReservationRequest.java
@@ -1,5 +1,6 @@
 package com.rameez.restaurant.api.request;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReservationRequest {
@@ -8,4 +9,39 @@ public class ReservationRequest {
 
     private String restaurantId;
 
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    public List<String> getDinerIds() {
+        return dinerIds;
+    }
+
+    public void setDinerIds(List<String> dinerIds) {
+        this.dinerIds = dinerIds;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
 }
--- src/main/java/com/rameez/restaurant/api/service/ReservationService.java
+++ src/main/java/com/rameez/restaurant/api/service/ReservationService.java
@@ -1,15 +1,36 @@
 package com.rameez.restaurant.api.service;
 
-import java.util.List;
+import com.rameez.restaurant.api.entity.Reservation;
+import com.rameez.restaurant.api.repository.ReservationRepository;
+import com.rameez.restaurant.api.request.ReservationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class ReservationService {
 
+    @Autowired
+    private ReservationRepository reservationRepository;
 
     public List<String> getAvailableReservations() {
         return null;
     }
 
-    public boolean createReservation() {
+    public boolean createReservation(ReservationRequest reservationRequest) {
+        LocalDateTime startTime = reservationRequest.getStartTime();
+        LocalDateTime endTime = reservationRequest.getEndTime();
+        String restaurantId = reservationRequest.getRestaurantId();
+        //get table Id from restaurantId
+        List<Reservation> reservations = new ArrayList<>();
+        for (String dinerId : reservationRequest.getDinerIds()) {
+            Reservation reservation = new Reservation();
+            reservations.add(reservation);
+        }
+        reservationRepository.createReservations(reservations);
         return false;
     }
 
